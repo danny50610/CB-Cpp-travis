e93701b18ef8dfbcff0b862ed44a66a1da104584
Chan, Danny <danny50610@yahoo.com.tw>
2017-12-13 16:46:10 +0800

    少;

--- GTest.cpp
+++ GTest.cpp
@@ -9,7 +9,7 @@ int addNumber(int a, int b) {
 #ifndef TEST
 int main() {
     printf("Hello, world\n");
-    printf("%d\n", addNumber(5, 2))
+    printf("%d\n", addNumber(5, 2));
     return 0;
 }
 #else
