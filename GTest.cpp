00fc538af6b495b24110be35fd1559a038bce293
Chan, Danny <danny50610@yahoo.com.tw>
2017-12-13 16:29:15 +0800

    Fix bug

--- GTest.cpp
+++ GTest.cpp
@@ -1,17 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 
 int addNumber(int a, int b) {
     return a + b;
 }
 
-int main()
-{
-    #ifdef TEST
-        assert(addNumber(5, 2) == 7);
-        exit(0);
-    #endif // TEST
-
+#ifndef TEST
+int main() {
     printf("Hello, world\n");
     return 0;
 }
+#else
+int main() {
+    assert(addNumber(5, 2) == 7);
+    exit(0);
+}
+#endif // TEST
+
